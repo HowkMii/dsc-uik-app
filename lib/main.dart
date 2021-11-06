3375d7e38bb8ac5c86db6c7b22f3687320ffe5a9
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-06 21:01:40 +0100

    ConcentricPageView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:concentric_transition/clipper.dart';
+import 'package:concentric_transition/concentric_transition.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,11 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'DSC IKU',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'DSC IKU'),
     );
   }
 }
@@ -31,39 +33,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: ConcentricPageView(
+        colors: [Colors.red, Colors.greenAccent, Colors.yellow, Colors.blue],
+        itemBuilder: (index, value) {
+          return Container();
+        },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
