d00519c3c7ab162aea1ca92547cef80ba1db1282
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-06 21:33:00 +0100

    day 1

--- lib/main.dart
+++ lib/main.dart
@@ -33,13 +33,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> pages = [
+    {
+      'image':
+          'https://ouch-cdn2.icons8.com/eW02ksQgD03vMcVQHZaqwFtb6reRJTvmEWPbixyFejc/rs:fit:392:392/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTg4/LzdhMzZlMDA3LWM4/OWQtNGVmOC04YjQ1/LTZhZWYzYjMxNGQ2/OC5wbmc.png',
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ConcentricPageView(
+        radius: 38,
+        verticalPosition: 0.8,
         colors: [Colors.red, Colors.greenAccent, Colors.yellow, Colors.blue],
         itemBuilder: (index, value) {
-          return Container();
+          int pageindex = (index % pages.length);
+          return Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  pages[pageindex]['image'],
+                  width: 300,
+                ),
+                Text(
+                  'We are DSC not GDG',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                )
+              ],
+            ),
+          );
         },
       ),
     );
